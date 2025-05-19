e81397a53e8685aed95df143b942ca82d1aa33f4
git-meywo <rubyboulon@proton.me>
2025-05-20 00:35:46 +0200

    added password creation and checking

---
+++ src/diskops.h
@@ -0,0 +1,9 @@
+# ifndef DISKOPS_H
+# define DISKOPS_H
+
+# include <stdio.h>
+# include <stdbool.h>
+
+bool file_exists(const char *filename);
+
+# endif // DISKOPS_H
---
+++ src/password.c
@@ -0,0 +1,35 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <string.h>
+
+void create_master_password(const char *password) {
+
+    FILE *file = fopen("masterpassword", "w");
+    if (file == NULL) {
+
+        printf("Failed creating file.\n");
+        exit(1);
+    }
+
+    fprintf(file, "%s", password);
+
+    fclose(file);
+}
+
+bool check_password(const char *password) {
+
+    char master_password[250];
+
+    FILE *file = fopen("masterpassword", "r");
+    if (file == NULL) {
+
+        printf("Failed opening file.\n");
+        exit(1);
+    }
+
+    fgets(master_password, sizeof(master_password), file);
+    fclose(file);
+
+    return strcmp(password, master_password) == 0;
+}
