693f9fb849a519fa43bc61577019244c655e46d2
git-meywo <rubyboulon@proton.me>
2025-05-22 00:34:20 +0200

    moved <string.h> one line up
    it was bothering me :/

--- include/password/password.c
+++ include/password/password.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <stdbool.h>
 # include <string.h>
+# include <stdbool.h>
 
 void create_master_password(const char *password) {
 
--- include/password/password.h
+++ include/password/password.h
@@ -3,8 +3,8 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-# include <stdbool.h>
 # include <string.h>
+# include <stdbool.h>
 
 void create_master_password(char *password);
 bool check_password(char *password);
