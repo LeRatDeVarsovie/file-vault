2746963a00920092b516d17abde7e3c724663ef3
git-meywo <rubyboulon@proton.me>
2025-05-21 23:34:09 +0200

    fixed typo

--- src/include/hash-lib.c
+++ src/include/hash-lib.c
@@ -5,17 +5,17 @@
 
 
 char* hash_function(char* password) {
-    uint64_t hash = 0xcbf29ce484222325; // 64-bit initial value for the hash  
+    uint64_t hash = 0xcbf29ce484222325; // 64-bit initial value for the hash
 
     for (int i = 0; i < strlen(password); i++) {
         hash ^= password[i];
-        hash *= 0x100000001b3; // Represent a specific prime number, his decimal value is 1099511628211
+        hash *= 0x100000001b3; // Represent a specific prime number, its decimal value is 1099511628211
         hash ^= (hash >> 32); // We simply mix the right 32 bits of the hash with the left 32 bits
     }
 
     char* result = (char*) malloc(9 * sizeof(char)); // We need to allocate a certain amount of memory on the heap (9 bytes) to store the final result
-    // in order to return a pointer to the character array containing the final hash 
-    
+    // in order to return a pointer to the character array containing the final hash
+
     if (result == NULL) { // In case of allocation error
         return NULL;
     }
@@ -23,7 +23,7 @@ char* hash_function(char* password) {
     memcpy(result, &hash, 8);
     result[8] = '\0';
 
-    return result; 
+    return result;
 }
 
 void display_ascii_hash(char* hash) {
@@ -32,5 +32,3 @@ void display_ascii_hash(char* hash) {
     }
     printf("\n");
 }
-
-
