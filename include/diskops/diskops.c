1dbc7272edb083c0a3d40a62b187f493ac195dc0
git-meywo <rubyboulon@proton.me>
2025-05-22 00:39:45 +0200

    added a new file_from_text function

--- include/diskops/diskops.c
+++ include/diskops/diskops.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <stdlib.h>
 # include <stdbool.h>
 
 bool file_exists(const char *filename) {
@@ -12,4 +13,23 @@ bool file_exists(const char *filename) {
 
         return false;
     }
+}
+
+void file_from_text(const char * filename, const char *content) {
+
+    if (file_exists(filename)) {
+
+        printf("File already exists.\n");
+        exit(1);
+    }
+
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+
+        printf("Failed creating file.\n");
+        exit(1);
+    }
+
+    fprintf(file, "%s", content);
+    fclose(file);
 }
