ee2b32a97d7fd64db56bb67d622c7a07f6904d3b
LaLangousteFolle <112510681+LaLangousteFolle@users.noreply.github.com>
2025-05-22 22:12:04 +0200

    Added header file for entry_management.h

---
+++ src/entry_management.h
@@ -0,0 +1,13 @@
+# ifndef ENTRY_MANAGEMENT_H
+# define ENTRY_MANAGEMENT_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+void create_entry(void);
+void modify_entry_value(void);
+void modif_entry_content(void);
+void delete_entry(void);
+
+# endif
